a04fda86f817e2fcf7d8be313bb85065c4c160d5
Manu <operamike@mail.ru>
2020-12-12 13:51:29 +0100

    feat: Add automatic router

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+const init = async () => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  const module = await import('vue-automatic-router')
+  const router = await module.default
+  createApp(App)
+    .use(router)
+    .mount('#app')
+}
+
+init().catch(() => {
+  console.log('Cannot start the app')
+})
